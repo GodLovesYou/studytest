606cd0b5c3317f2eb53e6e41ffe8f08bb0422466
GodLovesYou <1593370493@qq.com>
2019-09-12 17:17:45 +0800

    23123

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,65 @@
 <template>
   <div class="container">
-    <div>
-
-    </div>
+   <ele-calendar
+          lang="en"
+          :render-content="renderContent"
+          :data="datedef"
+          :prop="prop"
+    ></ele-calendar>
   </div>
 </template>
 
 <script>
+import eleCalendar from 'ele-calendar'
+import 'ele-calendar/dist/vue-calendar.css'
+import './style.scss'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      datedef:[
+          {"date":"2019-09-30","content":"1212","cid":"aaa"},
+          {"date":"2019-09-26","content":null,"cid":null},
+      ],
+      prop:'date' //对应日期字段名
+    }
+  },
+  components: {
+      eleCalendar
+  },
+  methods: {
+    renderContent(h,parmas) {
+      console.log(h,parmas)
+      const loop = data =>{
+        return (
+          data.defvalue.column===0||data.defvalue.column===6?
+          <div>
+            <div style="color:red;">休</div>
+            <div class="remark">
+              <div><span>{data.defvalue.text}</span></div>
+              <div>加班</div>
+            </div>
+          </div> 
+          : 
+          <div>
+            <div>班</div>
+            <div class="remark">
+              <div><span>{data.defvalue.text}</span></div>
+              <div>调休</div>
+            </div>
+          </div>
+        )
+      }
+      return (
+      <div>
+        {loop(parmas)}
+      </div>
+      );
+    },
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-  .container{
-    width: 200px;
-    height: 200px;
-    margin: 50px;
-    border: 1px solid;
-    line-height: 200px;
-    text-align: center;
-    div{
-      display: inline-block;
-      width: 100px;
-      height: 100px;
-      margin-left: 200px;
-      vertical-align: middle;
-      border-radius: 20px;
-      background: red;
-    }
-  }
 </style>
